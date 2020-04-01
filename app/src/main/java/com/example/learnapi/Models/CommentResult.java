aa759367fd289786201b01ffdd044541a57d1171
kubradurak <kubradurak.eng@gmail.com>
2020-04-02 02:27:06 +0300

    degisiklik

---
+++ app/src/main/java/com/example/learnapi/Models/CommentResult.java
@@ -0,0 +1,61 @@
+package com.example.learnapi.Models;
+
+public class CommentResult{
+	private String name;
+	private int postId;
+	private int id;
+	private String body;
+	private String email;
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void setPostId(int postId){
+		this.postId = postId;
+	}
+
+	public int getPostId(){
+		return postId;
+	}
+
+	public void setId(int id){
+		this.id = id;
+	}
+
+	public int getId(){
+		return id;
+	}
+
+	public void setBody(String body){
+		this.body = body;
+	}
+
+	public String getBody(){
+		return body;
+	}
+
+	public void setEmail(String email){
+		this.email = email;
+	}
+
+	public String getEmail(){
+		return email;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"CommentResult{" + 
+			"name = '" + name + '\'' + 
+			",postId = '" + postId + '\'' + 
+			",id = '" + id + '\'' + 
+			",body = '" + body + '\'' + 
+			",email = '" + email + '\'' + 
+			"}";
+		}
+}
